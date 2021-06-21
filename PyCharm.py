3b4f4d0d9ae174b404a3f274a9bf1a335b6df7c7
이주현 <84003327+leeeju@users.noreply.github.com>
2021-06-21 19:39:37 +0900

    응용문(1)

---
+++ PyCharm.py
@@ -0,0 +1,14 @@
+smax = int(input("큰수"))
+smin = int(input('작은수'))
+
+work1 = smax - smin
+work2 = smax * smin
+work3 = smax / smin
+work4 = smax + smin
+work5 = smax // smin
+
+print('큰수 - 작은수 : ', work1)
+print('큰수 * 작은수 : ', work2)
+print('큰수 / 작은수 : ', work3)
+print('큰수 + 작은수 : ', work4)
+print('큰수 // 작은수 : ', work5)
