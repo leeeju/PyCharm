a94f210fffb48e8bcd2f274388f9128497f1ba4d
이주현 <84003327+leeeju@users.noreply.github.com>
2021-06-22 20:09:09 +0900

    거북이 별 그리기

---
+++ turtle.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.shape('turtle')
+turtle.color('blue')
+
+# 거북이 별 그리기
+for i in range(5):
+    turtle.back(210)
+    turtle.right(100)
+    turtle.left(320)
+    turtle.left(280)
+    #turtle.forward(150)
+    #turtle.left(100)
+    #turtle.forward(60)
+    #turtle.left(100)
+    #turtle.right(120)
+    #turtle.back(90)
+turtle.mainloop()
