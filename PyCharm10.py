1546a5634520dbdd4f557bcb181fa393b10c69bb
이주현 <84003327+leeeju@users.noreply.github.com>
2021-06-22 20:10:14 +0900

    시간차 두고 출력하기

---
+++ PyCharm10.py
@@ -0,0 +1,16 @@
+# 1~10 까지 출력
+'''
+i = 1 # 초기화
+while i <= 10:
+    print(i)
+    i+=1
+'''
+import time
+
+for i in range(10,0,-1):
+    print(i , end=' ')
+    time.sleep(1)
+print('당첨입니다')
+
+
+# 시간차를 두고 출력하기
